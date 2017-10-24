0e93d2f4bff9579c2b7946b96471e48e94646252
Mark O Connor <markanthonyoconnor@gmail.com>
2017-10-24 20:01:28 +0100

    make it better

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
